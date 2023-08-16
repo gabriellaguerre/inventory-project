3c72a19f10ad7f24796e236a0b5ed7ea1e365615
gabriellaguerre <laguerre.g.g@gmail.com>
2023-08-16 12:52:33 -0400

    user refactored in signup and auth

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -39,7 +39,7 @@ def login():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
-        user = User.query.filter(User.email == form.data['email']).first()
+        user = User.query.filter(User.employeeID == form.data['employeeID']).first()
         login_user(user)
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
@@ -63,8 +63,8 @@ def sign_up():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         user = User(
-            username=form.data['username'],
-            email=form.data['email'],
+            employeeID=form.data['employeeID'],
+            accessLevel=form.data['accessLevel'],
             password=form.data['password']
         )
         db.session.add(user)
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -6,22 +6,22 @@ from app.models import User
 
 def user_exists(form, field):
     # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
+    # email = field.data
+    employeeID = field.data
+    user = User.query.filter(User.employeeID == employeeID).first()
     if user:
-        raise ValidationError('Email address is already in use.')
+        raise ValidationError('Employee ID is already in use.')
 
 
-def username_exists(form, field):
-    # Checking if username is already in use
-    username = field.data
-    user = User.query.filter(User.username == username).first()
-    if user:
-        raise ValidationError('Username is already in use.')
+# def username_exists(form, field):
+#     # Checking if username is already in use
+#     username = field.data
+#     user = User.query.filter(User.username == username).first()
+#     if user:
+#         raise ValidationError('Username is already in use.')
 
 
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    employeeID = StringField('employeeID', validators=[DataRequired(), user_exists])
+    accessLevel = StringField('accessLevel', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
