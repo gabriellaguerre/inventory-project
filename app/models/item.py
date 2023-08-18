14b97772f4f724826cb7b4909e2984c5a42e1945
gabriellaguerre <laguerre.g.g@gmail.com>
2023-08-17 20:12:43 -0400

    added items table

--- app/models/item.py
+++ app/models/item.py
@@ -9,7 +9,7 @@ class Item(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.Integer, nullable=False, unique=True)
-    description = db.Columm(db.String(500), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
     item_type = db.Column(db.String(500), nullable=False)
     unit_cost = db.Column(db.Integer, nullable=False)
     manufacturer = db.Column(db.String(255), nullable=False)
