7e70d3feb14e23f3a844d2f7e77af3603a9ce500
gabriellaguerre <laguerre.g.g@gmail.com>
2023-08-21 12:58:59 -0400

    changed item list to table css

--- react-app/src/components/ItemsPage/ItemsAdmin.js
+++ react-app/src/components/ItemsPage/ItemsAdmin.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import * as ItemsActions from '../../store/items'
+import './ItemsAdmin.css'
 
 function ItemsAdmin() {
     const dispatch = useDispatch()
@@ -10,21 +11,33 @@ function ItemsAdmin() {
     }, [dispatch])
 
     const items = useSelector(state => Object.values(state.items))
-  
+
 
     return (
         <>
         {/* <h2>Inventory</h2> */}
-        {items.map(item =>
-             <div key={item.id} className="item">
-             <div>Code: {item.code}</div>
-             <div>Description: {item.description}</div>
-             <div>Type: {item.item_type}</div>
-             <div>Unit Cost: ${item.unit_cost}</div>
-             <div>Quantity: {item.quantity}</div>
-             <div>Total Cost: ${item.unit_cost*item.quantity}</div>
-             <div>Manufacturer: {item.manufacturer}</div>
-             </div>)}
+    <table>
+        <tr>
+            <th>Code</th>
+            <th>Description</th>
+            <th>Type</th>
+            <th>Unit Value</th>
+            <th>Quantity</th>
+            <th>Total Value</th>
+            <th>Manufacturer</th>
+        </tr>
+             {items.map(item =>
+             <tr key={item.id} className="item">
+             <td>{item.code}</td>
+             <td>{item.description}</td>
+             <td>{item.item_type}</td>
+             <td>${item.unit_cost}</td>
+             <td>{item.quantity}</td>
+             <td>${item.unit_cost*item.quantity}</td>
+             <td>{item.manufacturer}</td>
+             </tr>)}
+
+    </table>
         </>
     )
 }
--- react-app/src/components/ItemsPage/ItemsEmp.js
+++ react-app/src/components/ItemsPage/ItemsEmp.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, { useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import * as ItemsActions from '../../store/items'
+import './ItemsEmp.css'
 
 function ItemsEmp() {
 
-    const items = useSelector(state => state.items)
-    console.log(items)
+    const dispatch = useDispatch()
+
+    useEffect(()=> {
+        dispatch(ItemsActions.getAllItems())
+    }, [dispatch])
+
+    const items = useSelector(state => Object.values(state.items))
+
 
     return (
-        <h2>Items Emp Page</h2>
+        <>
+        {/* <h2>Inventory</h2> */}
+    <table>
+        <tr>
+            <th>Code</th>
+            <th>Description</th>
+            <th>Type</th>
+            <th>Quantity</th>
+            <th>Manufacturer</th>
+        </tr>
+             {items.map(item =>
+             <tr key={item.id} className="item">
+             <td>{item.code}</td>
+             <td>{item.description}</td>
+             <td>{item.item_type}</td>
+             <td>{item.quantity}</td>
+             <td>{item.manufacturer}</td>
+             </tr>)}
+
+    </table>
+        </>
     )
+
 }
 
 export default ItemsEmp;
