3554591341273e126c80c5cbfe4ce39d5c8042e2
gabriel <gabriel.g.laguerre@gmail.com>
2023-10-09 13:10:56 -0400

    refactored seeds

--- app/seeds/purchase_order_items.py
+++ app/seeds/purchase_order_items.py
@@ -1,21 +1,25 @@
 from app.models import db, environment, SCHEMA, PurchaseOrderItems, Item, PurchaseOrder
 # db, Item, Supplier, environment,
 # from ..models.item import Item
-# from app.models.supplier_item import supplier_items
+from app.models.purchase_order_item import PurchaseOrderItems
 from sqlalchemy.sql import text
 
 
 
 def seed_purchase_order_items():
-    pos1 = PurchaseOrder.query.filter(PurchaseOrder.id == 1).first()
-    quantity1 = PurchaseOrderItems(quantity = 6)
-    quantity1.item = Item.query.filter(Item.id == 1).first()
-    pos1.items.append(quantity1)
+    # pos1 = PurchaseOrder.query.filter(PurchaseOrder.id == 1).first()
+    # quantity1 = PurchaseOrderItems(quantity = 6)
+    # quantity1.item = Item.query.filter(Item.id == 1).first()
+    # pos1.items.append(quantity1)
 
-    quantity2 = PurchaseOrderItems(quantity = 2)
-    quantity2.item = Item.query.filter(Item.id == 2).first()
-    pos1.items.append(quantity2)
+    # quantity2 = PurchaseOrderItems(quantity = 2)
+    # quantity2.item = Item.query.filter(Item.id == 2).first()
+    # pos1.items.append(quantity2)
 
+    po1 = PurchaseOrderItems(id =1, purchase_orderId = 1, itemId = 1, quantity = 6)
+    po2 = PurchaseOrderItems(id =2, purchase_orderId = 1, itemId = 2, quantity = 2)
+
+    db.session.add_all([po1, po2])
     db.session.commit()
 
     # item2 = Item.query.filter(Item.id == 2).first()
--- app/seeds/request_items.py
+++ app/seeds/request_items.py
@@ -1,23 +1,26 @@
 from app.models import db, environment, SCHEMA, RequestItems, Request, Item
 # db, Item, Supplier, environment,
 # from ..models.item import Item
-# from app.models.supplier_item import supplier_items
+from app.models.request_item import RequestItems
 from sqlalchemy.sql import text
 
 
 
 def seed_request_items():
 
-    req1 = Request.query.filter(Request.id == 1).first()
-    quantity1 = RequestItems(quantity = 4)
-    quantity1.item = Item.query.filter(Item.id == 1).first()
-    req1.items.append(quantity1)
+    # req1 = Request.query.filter(Request.id == 1).first()
+    # quantity1 = RequestItems(quantity = 4)
+    # quantity1.item = Item.query.filter(Item.id == 1).first()
+    # req1.items.append(quantity1)
 
 
-    quantity2 = RequestItems(quantity = 2)
-    quantity2.item = Item.query.filter(Item.id == 2).first()
-    req1.items.append(quantity2)
+    # quantity2 = RequestItems(quantity = 2)
+    # quantity2.item = Item.query.filter(Item.id == 2).first()
+    # req1.items.append(quantity2)
+    ri1 = RequestItems(id = 1, requestId = 1, itemId = 1, quantity = 4)
+    ri2 = RequestItems(id = 2, requestId = 1, itemId = 2, quantity = 2)
 
+    db.session.add_all([ri1, ri2])
     db.session.commit()
 
 
