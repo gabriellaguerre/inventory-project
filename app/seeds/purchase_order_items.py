cc6103ac2ecab2c9a7586659f23fa6671c011bd0
gabriel <gabriel.g.laguerre@gmail.com>
2023-10-10 10:05:49 -0400

    modified seeds

--- app/seeds/purchase_order_items.py
+++ app/seeds/purchase_order_items.py
@@ -7,19 +7,19 @@ from sqlalchemy.sql import text
 
 
 def seed_purchase_order_items():
-    # pos1 = PurchaseOrder.query.filter(PurchaseOrder.id == 1).first()
-    # quantity1 = PurchaseOrderItems(quantity = 6)
-    # quantity1.item = Item.query.filter(Item.id == 1).first()
-    # pos1.items.append(quantity1)
+    pos1 = PurchaseOrder.query.filter(PurchaseOrder.id == 1).first()
+    quantity1 = PurchaseOrderItems(quantity = 6)
+    quantity1.item = Item.query.filter(Item.id == 1).first()
+    pos1.items.append(quantity1)
 
-    # quantity2 = PurchaseOrderItems(quantity = 2)
-    # quantity2.item = Item.query.filter(Item.id == 2).first()
-    # pos1.items.append(quantity2)
+    quantity2 = PurchaseOrderItems(quantity = 2)
+    quantity2.item = Item.query.filter(Item.id == 2).first()
+    pos1.items.append(quantity2)
 
-    po1 = PurchaseOrderItems(id =1, purchase_orderId = 1, itemId = 1, quantity = 6)
-    po2 = PurchaseOrderItems(id =2, purchase_orderId = 1, itemId = 2, quantity = 2)
+    # po1 = PurchaseOrderItems(id =1, purchase_orderId = 1, itemId = 1, quantity = 6)
+    # po2 = PurchaseOrderItems(id =2, purchase_orderId = 1, itemId = 2, quantity = 2)
 
-    db.session.add_all([po1, po2])
+    # db.session.add_all([po1, po2])
     db.session.commit()
 
     # item2 = Item.query.filter(Item.id == 2).first()
