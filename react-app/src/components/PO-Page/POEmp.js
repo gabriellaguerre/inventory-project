84cd522fe2911697b62fcdf17bdf3f6a8dc1bda3
gabriel <gabriel.g.laguerre@gmail.com>
2024-03-05 17:46:34 -0500

    finished search PO for employee

--- react-app/src/components/PO-Page/POEmp.js
+++ react-app/src/components/PO-Page/POEmp.js
@@ -7,6 +7,7 @@ import ItemListPO from '../ItemsPage/ItemListPO';
 import NewRequestForm from '../NewRequestForm/NewRequestForm.js';
 import NewPOForm from '../NewPOForm/NewPOForm.js';
 import OpenModalButton from '../OpenModalButton';
+import SearchPOByDate from '../SearchPOByDate/SearchPOByDate.js'
 import './POEmp.css';
 
 
@@ -14,9 +15,19 @@ import './POEmp.css';
 function POEmp() {
     const dispatch = useDispatch()
 
-
     const [page, setPage] = useState(0)
     const [disable, setDisable] = useState(false)
+    const [query, setQuery] = useState('')
+    const [filter, setFilter] = useState('')
+    const [isSearching, setIsSearching] = useState(false)
+    const [chooseOpenPO, setChooseOpenPO] = useState(false)
+    const [chooseReceivedPO, setChooseReceivedPO] = useState(false)
+    const [chooseID, setChooseID] = useState(false)
+    const [chooseRangeDate, setChooseRangeDate] = useState(false)
+    const [chooseUserID, setChooseUserID] = useState(false)
+    const [searchDisabled, setSearchDisabled] = useState(false)
+    const [searchDates, setSearchDates] = useState({startDate: null, endDate: null})
+
 
     useEffect(() => {
         dispatch(UsersActions.get_Users())
@@ -44,19 +55,97 @@ function POEmp() {
         newPOs.push(po)
     }
 
-    // const previous = (page) => {
-    //     if (page>0) dispatch(POsActions.resetState())
-    //     previous(page)
-    //     dispatch(POsActions.resetState())
-    // }
+    const searchAction = async () => {
+        if (query && !filter && searchDisabled===false) {
+            alert('Please Choose A Filter.')
+        } else if (!query && filter && searchDisabled===false) {
+            alert('Please Fill Out The Search Field.')
+        } else if (!query && !filter) {
+            alert('All The Fields Are Empty.  Please Fill Them Out.')
+        } else if (filter === 'userId') {
+            handleCreatedBy()
+        }else {
+            dispatch(POsActions.resetState())
+            dispatch(POsActions.searchPOs({query, filter}))
+            setIsSearching(true)
+        }
+    }
+
+    const clearSearch = async () => {
+        setIsSearching(false)
+        setFilter('')
+        setQuery('')
+        setChooseOpenPO(false)
+        setChooseReceivedPO(false)
+        setChooseID(false)
+        setChooseRangeDate(false)
+        setChooseUserID(false)
+        setSearchDisabled(false)
+        setSearchDates({startDate: null, endDate: null})
+        dispatch(UsersActions.get_Users())
+        dispatch(POsActions.resetState())
+        dispatch(POsActions.getPOSByPage(page))
+    }
+
+    const chooseFilterOpenPO = 'search' + (chooseOpenPO ? "Yes" : "No")
+    const chooseFilterReceivedPO = 'search' + (chooseReceivedPO ? "Yes" : "No")
+    const chooseFilterID = 'search' + (chooseID ? "Yes" : "No")
+    const chooseFilterDate = 'search' + (chooseRangeDate ? "Yes" : "No")
+    const chooseFilterUserID = 'search' + (chooseUserID ? "Yes" : "No")
+
+    const handleSearchDate = (startDate, endDate) => {
+        setIsSearching(false)
+        setSearchDates({startDate, endDate})
+    }
+
+    const handleCreatedBy = () => {
+        const userArray = Object.values(user)
+        let newQuery = userArray.find(({ employeeID }) => employeeID === query)
+
+        if(newQuery) {
+            setIsSearching(true)
+            dispatch(POsActions.resetState())
+            dispatch(POsActions.searchPOs({query: newQuery.id, filter}))
+        } else {
+            alert('Either this user does not exist or there is a typo.  Please check spelling')
+        }
+
+    }
 
     return (
         <>
-            <div id='pagination'>
+            {isSearching ? (
+            <div id='isSearching'>Full List of Search Results</div>
+        ) : searchDates.startDate && searchDates.endDate ? (
+            <div id='isSearching' >Full List of Search Results between {searchDates.startDate} and {searchDates.endDate}</div>
+        ) : (<div id='pagination'>
             <button id='previous' onClick={()=> {if (page>0) setPage(page-1); }}>Previous</button>
             <span id='page'>Page {page+1} of {' '}{purchase_orders[purchase_orders.length-1]}</span>
-            <button id='next' onClick={()=> {setPage(page+1); }} disabled={disable}>Next</button>
+            <button id='next' onClick={()=> {setPage(page+1);}} disabled={disable}>Next</button>
             </div>
+        )}
+
+        <div className='search'>
+            <input id='search'
+             value={query}
+             placeholder='Choose a filter and type your search'
+             disabled={searchDisabled}
+             onChange={(e)=>setQuery(e.target.value)}
+             />
+             <button className='searchClear' onClick={()=>searchAction()}><i className="fa-solid fa-magnifying-glass"></i></button>
+             <button className='searchClear' onClick={()=>clearSearch()}><i className="fa-solid fa-broom"></i></button>
+
+        </div>
+        <div id='filter'>
+            Filter by: <button id={chooseFilterOpenPO} className='sidcButton' onClick={()=> {setFilter('receivedFalse'); setChooseOpenPO(true); setChooseReceivedPO(false); setChooseID(false); setChooseRangeDate(false); setChooseUserID(false); setSearchDisabled(true)}}>Open Puchase Orders</button>
+            <button id={chooseFilterReceivedPO} className='sidcButton' onClick={()=> {setFilter('receivedTrue'); setChooseOpenPO(false); setChooseReceivedPO(true); setChooseID(false); setChooseRangeDate(false); setChooseUserID(false); setSearchDisabled(true)}}>Received Puchase Orders</button>
+            <button id={chooseFilterID} className='sidcButton' onClick={()=> {setFilter('id'); setChooseOpenPO(false); setChooseReceivedPO(false); setChooseID(true); setChooseRangeDate(false); setChooseUserID(false); setSearchDisabled(false)}}>Purchase Order ID</button>
+            <button id={chooseFilterDate} className='sidcButton' onClick={()=> {setFilter('createdAt'); setChooseOpenPO(false); setChooseReceivedPO(false); setChooseID(false); setChooseRangeDate(true); setChooseUserID(false); setSearchDisabled(false)}}><OpenModalButton
+                                    buttonText=<div>Date</div>
+                                    modalComponent={<SearchPOByDate onDateSubmit={handleSearchDate}/>}/></button>
+            <button id={chooseFilterUserID} className='sidcButton' onClick={()=> {setFilter('userId'); setChooseOpenPO(false); setChooseReceivedPO(false); setChooseID(false); setChooseRangeDate(false); setChooseUserID(true); setSearchDisabled(false)}}>Created By</button>
+        </div>
+
             <table className='po-table-employee'>
                 <thead>
                     <tr>
