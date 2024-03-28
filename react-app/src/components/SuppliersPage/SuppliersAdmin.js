0b22164d6c359ad279d8c826fd306df7848f3951
gabriel <gabriel.g.laguerre@gmail.com>
2024-03-28 18:57:53 -0400

    started search query for suppliers admin

--- react-app/src/components/SuppliersPage/SuppliersAdmin.js
+++ react-app/src/components/SuppliersPage/SuppliersAdmin.js
@@ -15,6 +15,16 @@ import './SuppliersAdmin.css'
 
 function SuppliersAdmin() {
 const dispatch = useDispatch()
+const [page, setPage] = useState(0)
+const [disable, setDisable] = useState(false)
+const [query, setQuery] = useState('')
+const [filter, setFilter] = useState('')
+const [isSearching, setIsSearching] = useState(false)
+const [chooseName, setChooseName] = useState(false)
+const [chooseAddress, setChooseAddress] = useState(false)
+const [chooseContact, setChooseContact] = useState(false)
+const [chooseEmail, setChooseEmail] = useState(false)
+
 
 useEffect(()=> {
     dispatch(SuppliersActions.getSuppliers())
@@ -22,13 +32,84 @@ useEffect(()=> {
 
 const suppliers = useSelector(state => Object.values(state.suppliers))
 
+useEffect(()=> {
+    if ((page+2) > suppliers[suppliers.length-1]) {
+        setDisable(true)
+    } else {
+        setDisable(false)
+    }
+},[page, disable, suppliers])
+
+let newSuppliers = []
+for (let i = 0; i < (suppliers.length-1);i++) {
+    let supplier = suppliers[i]
+    newSuppliers.push(supplier)
+}
+
+
+const searchAction = async () => {
+    if (query && !filter) {
+        alert('Please Choose A Filter.')
+    } else if (!query && filter) {
+        alert('Please Fill Out The Search Field.')
+    } else if (!query && !filter) {
+        alert('All The Fields Are Empty.  Please Fill Them Out.')
+    } else {
+        dispatch(SuppliersActions.resetState())
+        dispatch(SuppliersActions.searchSuppliers({query, filter}))
+        setIsSearching(true)
+    }
+}
+
+const clearSearch = async () => {
+    setIsSearching(false)
+    setFilter('')
+    setQuery('')
+    setChooseName(false);
+    setChooseAddress(false);
+    setChooseContact(false);
+    setChooseEmail(false);
+    dispatch(SuppliersActions.resetState())
+    dispatch(SuppliersActions.getSuppliersByPage(page))
+}
+
+const chooseFilterName = 'search' + (chooseName ? "Yes" : "No")
+const chooseFilterAddress = 'search' + (chooseAddress ? "Yes" : "No")
+const chooseFilterContact = 'search' + (chooseContact ? "Yes" : "No")
+const chooseFilterEmail = 'search' + (chooseEmail ? "Yes" : "No")
 
 
 
 return (
     <>
      {/* <div>Suppliers</div> */}
-<table className='suppliers-table-admin'>
+     {(isSearching) ? (
+            <div id='isSearching'>Full List of Search Results</div>
+        ) : (
+            <div id='pagination'>
+            <button id='previous' onClick={()=> {if (page>0) setPage(page-1); }}>Previous</button>
+            <span id='page'>Page {page+1} of {' '}{items[items.length-1]}</span>
+            <button id='next' onClick={()=> {setPage(page+1);  }} disabled={disable}>Next</button>
+            </div>
+        )}
+
+        <div className='search'>
+            <input id='search'
+             value={query}
+             placeholder='Choose a filter and type your search'
+             onChange={(e)=>setQuery(e.target.value)}
+             />
+             <button className='searchClear' onClick={()=>searchAction()}><i className="fa-solid fa-magnifying-glass"></i></button>
+             <button className='searchClear' onClick={()=>clearSearch()}><i className="fa-solid fa-broom"></i></button>
+
+        </div>
+        <div id='filter'>
+            Filter by: <button id={chooseFilterCode} className='cdtButton' onClick={()=> {setFilter('code'); setChooseCode(true); setChooseDesc(false); setChooseType(false)}}>Code</button>
+            <button id={chooseFilterDesc} className='cdtButton' onClick={()=> {setFilter('description'); setChooseCode(false); setChooseDesc(true); setChooseType(false)}}>Description</button>
+            <button id={chooseFilterType} className='cdtButton' onClick={()=> {setFilter('type'); setChooseCode(false); setChooseDesc(false); setChooseType(true)}}>Type</button>
+
+        </div>
+  <table className='suppliers-table-admin'>
     <thead>
     <tr>
      <td className='header' style={{textAlign:'center'}} colSpan = '11'><button  id='requestForm'><OpenModalButton
